f588bb25af210c53ddd51dec4009aeb1a9c308df
Josh Kuhar <jdkuhar@gmail.com>
2016-07-31 14:48:39 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Model - function() {
+
+
+var Model = function() {
 	this.text = "";
 	this.onChange = null;
 };
@@ -9,3 +11,34 @@ Model.prototype.setText = function(value) {
 		this.onChange(this.text);
 	}
 };
+
+var View = function(elementSelector, initialValue) {
+	this.element = $(elementSelector);
+	this.setValue(initialValue || '');
+	this.onChange = null;
+
+	this.element.on('input', this.onInput.bind(this));
+};
+
+View.prototype.onInput = function() {
+	var value = this.element.val();
+	if (this.onChange) {
+		this.onChange(value);
+	}
+};
+
+View.prototype.setValue = function(text) {
+	this.element.val(text);
+};
+
+var Controller = function(model, view) {
+	view.onChange = model.setText.bind(model);
+	model.onChange = view.setValue.bind(view);
+};
+
+// This initializes the Model, View, and Controller
+document.addEventListener('DOMContentLoaded', function () {
+	var model = new Model();
+	var view = new View('.uppercase');
+	var controller = new Controller(model, view);
+});
